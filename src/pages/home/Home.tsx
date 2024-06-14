b27c397aa6ff5804b5aab15f6d6c1a43d3e1144a
Victor Cano <victorcano@Victors-MacBook-Pro.local>
2024-06-13 22:49:35 -0600

    feat(main): add service to get photos based on query

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,19 +2,42 @@ import SearchBar from '../../components/searchBar/SearchBar'
 import './Home.scss'
 import { useState } from 'react'
 import GradientBG from '../../assets/gradiend-bg@2x.png'
+import axios from 'axios'
 
-const Home = () => {
+interface Image {
+    id: string;
+    urls: {
+        regular: string;
+    };
+    alt_description: string;
+}
 
+interface Results {
+    results: Image[];
+}
+
+const Home: React.FC = () => {
     const [query, setQuery] = useState('');
     const [enterKey, setEnterKey] = useState(false)
+    const [results, setResults] = useState<Results | null>(null);
 
     const handleSearchChange = (newQuery: string) => {
         setQuery(newQuery);
     };
 
-    const handleSearch = () => {
-        // add api call with query...
-        console.log(query)
+    const handleSearch = async () => {
+        try {
+            const response = await axios.get(`https://api.unsplash.com/search/photos?page=1&query=${query}`, {
+                headers: {
+                    Authorization: `Client-ID ${import.meta.env.VITE_API_ACCESS_KEY}`
+                }
+            })
+            if (!response || response.status !== 200) return
+            setResults(response.data)
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+
     };
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -24,7 +47,7 @@ const Home = () => {
         }
     };
     return (
-        <div className={`h-full w-full ${!enterKey ? 'absoluteScreen backgroundImage' : 'relative'}`}>
+        <div className={`w-full ${!enterKey ? 'h-full absoluteScreen backgroundImage' : 'relative'}`}>
             {enterKey && <section>
                 <div>
                     <img className="w-screen h-28" src={GradientBG} alt='bg-gradient' />
@@ -37,6 +60,17 @@ const Home = () => {
                         <div className="text-base font-light pb-4">Searching high-resolution images from Unsplash</div>
                     </>}
                 <SearchBar onChange={handleSearchChange} onKeyDown={handleKeyDown} />
+            </section>
+            <section>
+                {
+                    results?.results
+                    &&
+                    <div className='relative grid grid-rows-2 grid-flow-col gap-5 justify-center p-5'>
+                        {results.results.map(image => (
+                            <img key={image.id} src={image.urls.regular} alt={image.alt_description} className="w-full object-cover" />
+                        ))}
+                    </div>
+                }
             </section>
         </div>
     )
