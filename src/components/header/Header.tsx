fbafa734e7b423f0f3cbc038202ab519eea23fa7
Victor Cano <victorcano@Victors-MacBook-Pro.local>
2024-07-19 20:57:45 -0600

    fix(main): remove useless code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,55 +1,14 @@
-import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import './Header.scss'
 import UnsplashIcon from '../../assets/Logo.svg'
 
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
-const drawerWidth = 240;
 const navItems = ['Home', 'Collections'];
 
-export default function Header(props: Props) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
-  };
-
-  const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
-      <Divider />
-      <List>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
-
+export default function Header() {
   return (
     <Box sx={{ display: 'flex', marginBottom: '3.8rem'}}>
       <CssBaseline />
@@ -58,7 +17,6 @@ export default function Header(props: Props) {
           <div
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerToggle}
           >
             <a href='/'><img src={UnsplashIcon} alt="unsplash-icon"></img></a>
           </div>
